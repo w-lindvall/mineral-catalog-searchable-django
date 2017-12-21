bdc9313cbc3c0ca8f0173ab8e90207934756b38f
William <lindvall.w@icloud.com>
2017-12-21 23:42:54 +0100

    Make layout and index html and make home page view

--- minerals/views.py
+++ minerals/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Mineral
+
+
+def home_page(request):
+    """View displays list of mineral names with links"""
+    minerals = Mineral.objects.all()
+    return render(request, 'index.html', {'minerals': minerals})
