16eff7c4ff88530758018e978f037796a662887c
William <lindvall.w@icloud.com>
2017-12-23 01:08:58 +0100

    Add debug toolbar

--- mineralsite/settings.py
+++ mineralsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'minerals',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mineralsite.urls'
@@ -120,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+INTERNAL_IPS = ['127.0.0.1',]
--- mineralsite/urls.py
+++ mineralsite/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 
+if settings.DEBUG:
+    import debug_toolbar
+
 urlpatterns = [
+    url(r'^__debug__/', include(debug_toolbar.urls)),
     url(r'', include('minerals.urls')),
     path('admin/', admin.site.urls),
 ]
