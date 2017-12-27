8109b3b74ebf126701017ff2e280f7991054a2a0
William <lindvall.w@icloud.com>
2017-12-27 15:36:12 +0100

    Add random mineral functionality

--- minerals/views.py
+++ minerals/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+import random
 
 from .models import Mineral
 
@@ -10,14 +11,18 @@ def home_page(request):
     groups = ['Silicates', 'Oxides', 'Sulfates', 'Sulfides', 'Carbonates',
               'Halides', 'Sulfosalts', 'Phosphates', 'Borates', 'Organic Minerals',
               'Arsenates', 'Native Elements', 'Other']
+    all_minerals = Mineral.objects.all()
+    random_mineral = random.choice(all_minerals)
     minerals = Mineral.objects.all().values('name', 'pk')
-    return render(request, 'index.html', {'minerals': minerals, 'groups': groups})
+    return render(request, 'index.html', {'minerals': minerals, 'groups': groups, 'random': random_mineral})
 
 
 def mineral_detail(request, pk):
     """View displays individual mineral details"""
+    all_minerals = Mineral.objects.all()
+    random_mineral = random.choice(all_minerals)
     mineral = get_object_or_404(Mineral, pk=pk)
-    return render(request, 'mineral_detail.html', {'mineral': mineral})
+    return render(request, 'mineral_detail.html', {'mineral': mineral, 'random': random_mineral})
 
 
 def mineral_name_search(request):
@@ -26,9 +31,11 @@ def mineral_name_search(request):
     groups = ['Silicates', 'Oxides', 'Sulfates', 'Sulfides', 'Carbonates',
               'Halides', 'Sulfosalts', 'Phosphates', 'Borates', 'Organic Minerals',
               'Arsenates', 'Native Elements', 'Other']
+    all_minerals = Mineral.objects.all()
+    random_mineral = random.choice(all_minerals)
     minerals = Mineral.objects.filter(name__icontains=term).values('name',
                                                                    'pk')
-    return render(request, 'index.html', {'minerals': minerals, 'groups': groups})
+    return render(request, 'index.html', {'minerals': minerals, 'groups': groups, 'random': random_mineral})
 
 
 def mineral_group_search(request, group):
@@ -36,8 +43,10 @@ def mineral_group_search(request, group):
     groups = ['Silicates', 'Oxides', 'Sulfates', 'Sulfides', 'Carbonates',
               'Halides', 'Sulfosalts', 'Phosphates', 'Borates', 'Organic Minerals',
               'Arsenates', 'Native Elements', 'Other']
+    all_minerals = Mineral.objects.all()
+    random_mineral = random.choice(all_minerals)
     minerals = Mineral.objects.filter(group__contains=group).values('name', 'pk')
-    return render(request, 'index.html', {'minerals': minerals, 'active_group': group, 'groups': groups})
+    return render(request, 'index.html', {'minerals': minerals, 'active_group': group, 'groups': groups, 'random': random_mineral})
 
 
 def mineral_first_search(request, letter='a'):
@@ -47,5 +56,7 @@ def mineral_first_search(request, letter='a'):
     groups = ['Silicates', 'Oxides', 'Sulfates', 'Sulfides', 'Carbonates',
               'Halides', 'Sulfosalts', 'Phosphates', 'Borates', 'Organic Minerals',
               'Arsenates', 'Native Elements', 'Other']
+    all_minerals = Mineral.objects.all()
+    random_mineral = random.choice(all_minerals)
     minerals = Mineral.objects.filter(name__istartswith=letter).values('name', 'pk')
-    return render(request, 'index.html', {'minerals': minerals, 'active_letter': letter, 'groups': groups})
+    return render(request, 'index.html', {'minerals': minerals, 'active_letter': letter, 'groups': groups, 'random': random_mineral})
