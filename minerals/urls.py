ccb1d29615595b8de622c7b47d1be9de474b5865
William <lindvall.w@icloud.com>
2017-12-27 15:36:25 +0100

    Change letter search url

--- minerals/urls.py
+++ minerals/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.mineral_first_search, name='home_page'),
     url(r'detail/(?P<pk>\d+)/$', views.mineral_detail, name='mineral_detail'),
     url(r'search/name$', views.mineral_name_search, name='mineral_name_search'),
-    url(r'letter/(?P<letter>[a-zA-Z])/$', views.mineral_first_search, name='mineral_first_search'),
+    url(r'search/letter/(?P<letter>[a-zA-Z])/$', views.mineral_first_search, name='mineral_first_search'),
     url(r'search/group/(?P<group>[a-zA-Z\s]+)/$', views.mineral_group_search, name='mineral_group_search'),
 ]
